1f5afc3b83d42d3a8473418f7d01dea9ac20211f
kiddapollo <kirbykidd46@gmail.com>
2022-12-16 00:52:54 -0500

    3rd-commit
    added functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, Directive, HostListener, Input, Self } from '@angular/core';
 import { discardPeriodicTasks } from '@angular/core/testing';
+import { TodosComponent } from './todos/todos.component';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,18 @@ import { discardPeriodicTasks } from '@angular/core/testing';
 export class AppComponent {
   title = 'todo-app';
 
+  todos: any[] = [];
+  completedTodos: any[] = [];
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+  // Add task to the todo-list
+  addTask(text: string): void {
+    this.todos.push();
+  }
 }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { discardPeriodicTasks } from '@angular/core/testing';
+import { CompletedComponent } from '../completed/completed.component';
 
 @Component({
   selector: 'app-todos',
@@ -7,5 +9,32 @@ import { Component } from '@angular/core';
 })
 export class TodosComponent {
 
+  //Functions
+  todos: any[] = [];
+  completedTodos: any[] = [];
+  editedTask: any;
+  edit = false;
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+  // function to move the todo task to the completed list
+  completeTask(task: any) {
+    this.todos = this.todos.filter(t => t !== task);
+    this.completedTodos.push(task);
+  }
   
+  // function to edit task if mispell or any erros
+  editTask(task: any) {
+    this.edit = true;
+    this.editedTask = task;
+  }
+  // function to delete task if decided not to be on the list anymore
+  deleteTask(task: any) {
+    const index = this.todos.indexOf(task);
+    this.todos.splice(index, 1);
+  }
+
 }
